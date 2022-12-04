dfa1c501c9b975f47261d2b44159a700d2898b8e
muhammadabidd <muhammmadabidd@gmail.com>
2022-12-04 23:14:58 +0700

    making nn model

--- API/app.py
+++ API/app.py
@@ -1,7 +1,7 @@
 
 
 import pickle
-from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer
 from keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
